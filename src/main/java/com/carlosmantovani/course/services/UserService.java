1d289d4dbdbff2ed067c9c87f696d6478d1c9977
carlos <sherifinho.augusto@gmail.com>
2025-05-24 12:44:24 -0300

    Added service layer and user GET requests

---
+++ src/main/java/com/carlosmantovani/course/services/UserService.java
@@ -0,0 +1,25 @@
+package com.carlosmantovani.course.services;
+
+import com.carlosmantovani.course.entities.User;
+import com.carlosmantovani.course.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.Optional;
+
+@Component
+public class UserService {
+
+    @Autowired
+    private UserRepository repository;
+
+    public List<User> findAll() {
+        return repository.findAll();
+    }
+
+    public User findById(Long id) {
+        Optional<User> obj = repository.findById(id);
+        return obj.get();
+    }
+}
